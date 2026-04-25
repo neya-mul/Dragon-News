e35893290aef2220a4ce7e238fb89637f68a7a24
neya-mul <neyamulislam946@gmail.com>
2026-04-25 20:02:23 +0600

    header added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import logo from "../assets/logo.png"
+import Image from 'next/image'
+import { format } from 'date-fns'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className=' text-center py-10 space-y-2'>
+      <Image src={logo} alt='logo' width={400} height={400} className=' mx-auto' />
+      <p className='text-xl font-bold'>Journalism Without Fear or Favour</p>
+      <p>{format(new Date(), "EEEE, MMM, dd, YYY")}</p>
+
+    </div>
   )
 }
