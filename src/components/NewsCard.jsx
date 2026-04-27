84a956844ae2bf094673830f7c96186242178433
neya-mul <neyamulislam946@gmail.com>
2026-04-27 11:27:46 +0600

    card details showed

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,26 +1,37 @@
 import Image from 'next/image'
 import React from 'react'
+import { CiBookmark, CiShare2 } from "react-icons/ci";
+
 
 export default function NewsCard({ n }) {
     return (
         <div className="card bg-base-100 shadow-sm">
 
             <div>
-                <div>
-                    {/* <Image src={n.author.img} width={50} height={50} alt='author' /> */}
+                <div className='flex justify-between items-center'>
+                    <div className='flex items-center gap-5'>
+                        <Image src={n.author.img} width={50} height={50} alt='author' className='rounded-full' />
+                        <div>
+                            <p>{n.author.name}</p>
+                            <p>{n.author?.published_date}</p>
+                        </div>
+                    </div>
+
+                    <div className='flex gap-2'>
+                        <CiShare2 />
+                        <CiBookmark />
+                    </div>
                 </div>
             </div>
             <figure>
-                <img
-                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                    alt="Shoes" />
+                <Image src={n.image_url} width={800} height={800} className='w-full h-auto object-fit ' alt='image' />
             </figure>
+         
             <div className="card-body">
-                <h2 className="card-title">Card Title</h2>
-                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
-                </div>
+             
+                <h1 className='text-2xl font-bold'>{n.title}</h1>
+                <p>{n.details}</p>
+               
             </div>
         </div>
     )
