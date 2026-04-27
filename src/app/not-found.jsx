b247e27f041963d0072aef69952a091cfdbfbc92
neya-mul <neyamulislam946@gmail.com>
2026-04-27 19:37:54 +0600

    not-found page added

---
+++ src/app/not-found.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function notFound() {
+  return (
+    <div className='flex justify-center items-center pt-100'>
+
+        <h1 className='text-4xl font-bold'>Page is not avalable</h1>
+    </div>
+  )
+}
