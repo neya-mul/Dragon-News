fbcb80d17b6edcc8b869497ec5d35eaca94f7dc0
neya-mul <neyamulislam946@gmail.com>
2026-04-28 09:35:22 +0600

    login form added

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,7 +1,65 @@
+'use client'
+import Link from 'next/link'
 import React from 'react'
 
 export default function Login() {
+  const loginFunction = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value
+  }
+
   return (
-    <div>Login</div>
+    <div>
+      <div className="min-h-screen flex items-center justify-center bg-black">
+        <div className="w-full max-w-md bg-zinc-900 p-8 rounded-2xl shadow-lg border border-zinc-800">
+
+          {/* Title */}
+          <h2 className="text-2xl font-semibold text-white text-center mb-6">
+            Welcome Back
+          </h2>
+
+          {/* Form */}
+          <form className="space-y-5" onSubmit={loginFunction}>
+
+            {/* Email Field */}
+            <div>
+              <label className="block text-sm text-gray-400 mb-1">
+                Email
+              </label>
+              <input
+                name='email'
+                type="email"
+                placeholder="you@example.com"
+                className="w-full px-4 py-2 rounded-lg bg-black border border-zinc-700 text-white placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-white focus:border-transparent transition"
+              />
+            </div>
+
+            {/* Password Field */}
+            <div>
+              <label className="block text-sm text-gray-400 mb-1">
+                Password
+              </label>
+              <input
+                name='password'
+                type="password"
+                placeholder="••••••••"
+                className="w-full px-4 py-2 rounded-lg bg-black border border-zinc-700 text-white placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-white focus:border-transparent transition"
+              />
+            </div>
+
+            {/* Button */}
+            <button
+              type="submit"
+              className="w-full py-2 rounded-lg bg-white text-black font-medium hover:bg-gray-200 transition"
+            >
+              Sign In
+            </button>
+            <p>Don't have an account? <Link href='/register' className='text-blue-500'>Register now</Link></p>
+          </form>
+
+        </div>
+      </div>
+    </div>
   )
 }
